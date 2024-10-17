5a905c282874867c961bd1d316007716f6b40a76
EricAG09 <33120444+EricAG09@users.noreply.github.com>
2024-10-17 16:35:48 -0300

    Criado formulario de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { LoginComponent } from "./views/login/login.component";
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardModule],
+  imports: [RouterOutlet, CardModule, InputTextModule, LoginComponent, ButtonModule],  
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Bem vindo ao frontend';
